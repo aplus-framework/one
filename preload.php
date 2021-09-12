06cb0004517e35ddbbfaf8ef7bc8f879ab47b13f
Natan Felles <natanfelles@gmail.com>
2021-09-12 04:39:40 -0300

    Add file for opcache.preload

---
+++ preload.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * This file is part of Aplus Framework One Project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+require __DIR__ . '/vendor/aplus/autoload/src/Preloader.php';
+
+use Framework\Autoload\Preloader;
+
+(new Preloader())->load();
